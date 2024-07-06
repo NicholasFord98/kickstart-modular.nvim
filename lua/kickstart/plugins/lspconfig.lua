1d0e549823c26b3ac036391682770a5a4ed484c6
Nicholas Ford <nicholasford98@yahoo.com>
2024-07-06 12:05:56 -0500

    added dartls, changed some options

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -156,19 +156,9 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        clangd = {},
+        rust_analyzer = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -192,6 +182,9 @@ return {
       --
       --  You can press `g?` for help in this menu.
       require('mason').setup()
+
+      -- custom setup for dartls - not sure why this has to be separate from `servers` above
+      require('lspconfig').dartls.setup {}
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.

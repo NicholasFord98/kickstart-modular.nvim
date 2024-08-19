b4df1c8533816ea85e9e4e0ce067d0dc6045a405
Nicholas Ford <nicholasford98@yahoo.com>
2024-08-19 14:44:40 -0500

    conflict resolution update

---
+++ lua/custom/plugins/options.lua
@@ -0,0 +1,10 @@
+-- set tab size
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+-- set thick cursor in most modes, blinking thick in others
+vim.opt.guicursor = 'n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:block-blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+
+-- disable line wrap
+vim.opt.wrap = false
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -166,10 +166,11 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+        cpptools = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -202,6 +203,9 @@ return {
       --
       --  You can press `g?` for help in this menu.
       require('mason').setup()
+
+      -- dartls
+      require('lspconfig').dartls.setup {}
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
